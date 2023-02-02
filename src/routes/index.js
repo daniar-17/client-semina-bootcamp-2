119406ee0ea38eabdaec648242af0ee18b910246
daniar-17 <daniarnuramin1@gmail.com>
2023-02-02 16:34:11 +0700

    84 Event Part 1

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ import { TalentsRoute } from "./TalentsRoute";
 import { CategoriesRoute } from "./CategoriesRoute";
 import { PaymentsRoute } from "./PaymentsRoute";
 import SNavbar from "../components/Navbar";
-// import { EventsRoute } from "./EventsRoute";
+import { EventsRoute } from "./EventsRoute";
 // import { OrdersRoute } from "./OrdersRoute";
 
 export function AppRoutes() {
@@ -35,7 +35,7 @@ export function AppRoutes() {
         <Route path="categories/*" element={<CategoriesRoute />} />
         <Route path="talents/*" element={<TalentsRoute />} />
         <Route path="payments/*" element={<PaymentsRoute />} />
-        {/* <Route path="events/*" element={<EventsRoute />} /> */}
+        <Route path="events/*" element={<EventsRoute />} />
         {/* <Route path="orders/*" element={<OrdersRoute />} /> */}
         <Route path="" element={<Navigate to="/dashboard" replace={true} />} />
       </Route>
