e8328eee4315dcfd30502a7e9ec491d80c901aaa
daniar-17 <daniarnuramin1@gmail.com>
2023-02-09 16:35:19 +0700

    86 Event Part 3

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
